85226c1e5b59379941f75e8413c970cd86a8830b
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-05-31 19:33:12 +0700

    Day 23: ES Module

---
+++ Day23/main.js
@@ -0,0 +1,6 @@
+import tinhTong, { add, PI } from "./math.js";
+
+console.log(PI); // 3.14159
+console.log(add(2, 3)); // 5
+
+console.log(tinhTong(10, 20));
---
+++ Day23/math.js
@@ -0,0 +1,10 @@
+const PI = 3.14159;
+function add(x, y) {
+	return x + y;
+}
+
+export default function sum(x, y) {
+	return x + y;
+}
+
+export { PI, add };
